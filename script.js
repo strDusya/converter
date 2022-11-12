25ae089badda405d30b0d51fbc0e6187de691100
Lev <115567936+strDusya@users.noreply.github.com>
2022-11-12 21:39:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-const rim = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+const rim = [1000, 900, 500, 400, 100,
+             90, 50, 40, 10, 9, 5, 4, 1]
 
-const rim1 = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+const rim1 = ['M', 'CM', 'D', 'CD', 'C', 'XC',
+             'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
 let numberInput = document.querySelector('.numberInput')
-let convertButton = document.querySelector('.numberButton')
-let result = document.querySelector('.result')
+let RomanInput = document.querySelector('.romanInput')
+let convertButtonNumber = document.querySelector('.numberButton')
+let convertButtonRoman = document.querySelector('.RomanButton')
+let resultRoman = document.querySelector('.resultRoman')
+let resultNumber = document.querySelector('.resultNumber')
 
 let userIinput = 0
-let useranswer = ''
-let index = 0
+let useranswerSTR = ''
+let useranswerINT = 0
 
-convertButton.addEventListener('click', function(useranswer){
+
+
+
+convertButtonNumber.addEventListener('click',(useranswerSTR) => {
     userIinput = numberInput.value
+    let index = 0
     
     if (userIinput > 3999) {
         result.innerHTML = 'Допустимая область значений: [1 ... 3999]';
@@ -19,14 +28,40 @@ convertButton.addEventListener('click', function(useranswer){
         while (userIinput > 0) {
             if(rim[index] <= userIinput){
                 userIinput = userIinput - rim[index]
-                useranswer = useranswer + rim1[index]
+                useranswerSTR = useranswerSTR + rim1[index]
             } else {
                 index = index + 1
             }
         }
-    
         index = 0
-        userIinput = 0
-        result.innerHTML = useranswer.substring(21, )
+        resultRoman.innerHTML = useranswerSTR.substring(21, )
     }
 } )
+
+
+
+convertButtonRoman.addEventListener('click',(useranswerINT) => {
+    userIinput = RomanInput.value.split('')
+    let len = (userIinput).length
+    let index = 0
+    let summ = 0
+
+    while ((userIinput).length > index) {
+        if(index + 1 > len){
+            useranswerINT = useranswerINT + rim[rim1.indexOf(userIinput[index])]
+        } else {
+            if (rim[rim1.indexOf(userIinput[index])] < rim[rim1.indexOf(userIinput[index + 1])]) {
+                summ = summ + Number(rim[rim1.indexOf(userIinput[index + 1])]) - Number(rim[rim1.indexOf(userIinput[index])])
+                index = index + 2
+            } else {
+                summ = summ + Number(rim[rim1.indexOf(userIinput[index])])
+                index = index + 1
+            }
+            
+        }
+    }
+    useranswerINT = summ
+    summ = 0
+    index = 0
+    resultNumber.innerHTML = useranswerINT
+})
